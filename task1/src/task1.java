f45070f566505c32d495205bec4fe42d02015729
TheAlexiy <86391263+TheAlexiy@users.noreply.github.com>
2021-07-04 16:31:39 +0400

    Serious changes
    Перелопачена большая часть программы:
    1) Сигнатура методов приведена к тем, что требуются в задании.
    2) Система счисления теперь подается в виде строки.
    3) Введена "защита от дурака"(usage?), которая обрабатывает входные данные и не даёт упасть программе.
    4) Другие незначительные изменения и оптимизации.

--- task1/src/task1.java
+++ task1/src/task1.java
@@ -5,29 +5,43 @@ public class task1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Введите число");
-        int a = scan.nextInt();
-        System.out.println("Введите исходную систему счисления");
-        int b = scan.nextInt();
-        System.out.println("Введите конечную систему счисления");
-        int c = scan.nextInt();
-        if ((b<2)|(c<2)) {
-            System.out.println("Ситстема счисления выбрана некорректно");
-            System.exit(0);
+        String a = scan.nextLine();
+        System.out.println("Введите исходную систему счисления (Например, «01» - двоичная, «012» - троичная, " +
+                "«0123456789abcdef» - шеснадцатиричная)");
+        String b = scan.nextLine();
+        if (check(a, b)) {
+            System.out.println("Введите конечную систему счисления");
+            String c = scan.nextLine();
+            System.out.println("Результат перевода");
+            if (b.equals("0123456789")) {
+                int nb = Integer.parseInt(a);
+                System.out.println(func(nb, c));
+            }
+            else System.out.println(func(a, b, c));
         }
-        System.out.println("Результат перевода");
-        if (b == 10) {
-            System.out.println(Func(a,c));
-        }
-        else {
-            System.out.println(Func(a,b,c)); //Перегрузка метода Func
-        }
+        else System.out.println("Число невозможно представить в данной системе счисления");
     }
-    public static String Func(int number, int radixFinal) {
-        BigInteger a= new BigInteger(String.valueOf(number));
-        return a.toString(radixFinal);
+    public static String func(int number, String toBase) {
+        int radix = toBase.length();
+        BigInteger a = new BigInteger(String.valueOf(number));
+        return a.toString(radix);
     }
-    public static String Func(int number, int radixBase, int radixFinal) {
-        BigInteger a= new BigInteger(String.valueOf(number), radixBase);
-        return a.toString(radixFinal);
+    public static String func(String number, String base, String toBase) {
+        int radix1 = base.length();
+        int radix2 = toBase.length();
+        BigInteger a = new BigInteger(String.valueOf(number), radix1);
+        return a.toString(radix2);
+    }
+    public static boolean check(String number, String toBase) {
+        String[] array = number.split("");
+        boolean result = false;
+        for (String s : array) {
+            if (toBase.contains(s)) result = true;
+            else {
+                result = false;
+                break;
+            }
+        }
+        return result;
     }
 }
