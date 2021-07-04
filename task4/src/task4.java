ddff5741a35c42da19fd7ae6600dda3b3158c6be
TheAlexiy <86391263+TheAlexiy@users.noreply.github.com>
2021-07-04 20:25:09 +0400

    Minor changes
    Изменения:
    1) Осуществлена передача входных данных через аргументы командной строки.
    2) Создан jar файл

--- task4/src/task4.java
+++ task4/src/task4.java
@@ -1,14 +1,15 @@
-import java.util.Scanner;
-
 public class task4 {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Введите первую строку");
-        String a = scan.nextLine();
-        System.out.println("Введите вторую строку");
-        String b = scan.nextLine();
-        String s = b.replaceAll("\\*+", ".*");
-        if ((a.matches(s))) {
+        StringBuilder s = new StringBuilder();
+        for (String arg : args) s.append(arg);
+        int index1 = s.indexOf("Первая строка:");
+        int index2 = s.indexOf(",");
+        String str1 = s.substring(s.indexOf("'",index1) + 1, s.indexOf(",",index1)-1);
+        System.out.println(str1);
+        String str2 = s.substring(s.indexOf("'",index2) + 1, s.indexOf(".",index2)-1);
+        System.out.println(str2);
+        String sub = str2.replaceAll("\\*+", ".*");
+        if ((str1.matches(sub))) {
             System.out.println("OK");
         }
         else {
